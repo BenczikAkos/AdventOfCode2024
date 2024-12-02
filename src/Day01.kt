7c75b3f3924ab782a674c3917adc27937189b709
Akos <benczikakos@gmail.com>
2024-12-02 16:30:48 +0100

    Day01 completed

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,33 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun List<String>.splitBySpace(): Pair<MutableList<Int>, MutableList<Int>> {
+        val mList1 = mutableListOf<Int>()
+        val mList2 = mutableListOf<Int>()
+        this.forEach {
+            mList1.add(it.split(" ").first().toInt())
+            mList2.add(it.split(" ").last().toInt())
+        }
+        return mList1 to mList2
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = input.splitBySpace()
+        return list1.sorted().zip(list2.sorted()) { a, b -> abs(a - b) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        //sum(every unique number * nbOccurencesInLeft * nbOccurencesRight)
+        val (list1, list2) = input.splitBySpace()
+        val uniqueNumbers: Set<Int> = list1.toSet()
+        return uniqueNumbers.sumOf { unique ->
+            unique *
+                    list1.map { if (it == unique) 1 else 0 }.sum() *
+                    list2.map { if (it == unique) 1 else 0 }.sum()
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
