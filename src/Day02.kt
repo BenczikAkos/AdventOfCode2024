86b9622917f78ea8e4b30d003f092ec765f626df
Akos <benczikakos@gmail.com>
2024-12-07 22:22:05 +0100

    Day02 part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -7,15 +7,24 @@ fun main() {
         return 1
     }
 
+    fun List<Int>.isSafeWithoutOne() : Int {
+        for (i in indices) {
+            val copy = this.toMutableList()
+            copy.removeAt(i)
+            if (copy.isSafe() == 1) return 1
+        }
+        return 0
+    }
+
     fun part1(input: List<String>): Int {
         return input.sumOf {report -> report.split(" ").map{ it.toInt() }.isSafe() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {report -> report.split(" ").map{ it.toInt() }.isSafeWithoutOne() }
     }
 
     val input = readInput("Day02")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
